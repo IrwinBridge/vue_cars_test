b25dfb372992fd4ee64b5478eca02a43ecaab826
IrwinBridge <jeffersoncity95@gmail.com>
2019-04-16 13:24:48 +0300

    initial

---
+++ src/components/CarCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {
+    name: 'car-card',
+  },
+  props: {
+    car: Object,
+  },
+}
+</script>
---
+++ src/components/CardGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Sorting buttons -->
+    <car-card v-for="car in cars" :key="car.id" :car="car"></car-card>
+  </div>
+</template>
+
+<script>
+import CarCard from '@/components/CarCard.vue';
+
+export default {
+  components: {
+    name: 'card-grid',
+    CarCard,
+  },
+};
+</script>
